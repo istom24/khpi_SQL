1e629a6e63c4257b1bc778a3e11bac8b13ba8a87
Yaraslava <slavaistom2006@gmail.com>
2024-09-27 18:25:29 +0300

    lab3

--- lab3.sql
+++ lab3.sql
@@ -1,16 +1,25 @@
 -- 1.
-SELECT name
+SELECT name 
 FROM cities
 WHERE name LIKE '%ськ';
-
 -- 2.
-SELECT name
+SELECT name 
 FROM cities
 WHERE name LIKE '%донец%';
-
-
---3.
-SELECT Concat (name, ' (',region, ')'),population
+-- 3.
+SELECT CONCAT(name, ' (', region, ')'), population
 FROM cities
 WHERE population > 100000
 ORDER BY name ASC;
+-- 4.
+SELECT name, population, (population / 40000000) * 100 AS pop_per
+FROM cities
+ORDER BY population DESC
+LIMIT 10;
+-- 5. 
+SELECT CONCAT(name, ' - ', (population / 40000000) * 100, '%') 
+FROM cities
+WHERE ((population / 40000000) * 100) >=0.1
+ORDER BY population ASC;
+
+
